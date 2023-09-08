d31b8aedf2036c5d73d01730b2d2599465fbbc07
Sergio Limas <sflimas2@misena.edu.co>
2023-09-07 20:36:21 -0500

    Se crea la conexión a la BD azure

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,12 +1,16 @@
 <?php
-
-    class DataBase{		
-		public function __construct(){}
-		public static function connection(){
-			$pdo = new PDO('mysql:host=localhost;dbname=saia;charset=utf8','root','');
+    class DataBase{
+        public static function connection(){            
+            $hostname = "saia.mysql.database.azure.com";
+            $port = "3306";
+            $database = "saia";
+            $username = "saia";
+            $password = "adsi2023*";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/DigiCertGlobalRootCA.crt.pem'
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
 	}
-
-?>
