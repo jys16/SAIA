195cf805b56af5663699f3db37fc1ff4e4777428
jys16 <sflimas2@misena.edu.co>
2023-09-12 18:44:12 -0500

    Prueba inicio sesión

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -9,10 +9,11 @@
             }
         }
         public function index(){
-            $session = $_SESSION['session'];                
+                $session = $_SESSION['session'];                
                 $userDto = unserialize($_SESSION['profile']);                
                 require_once "views/roles/" . $session . "/header.php";               
                 require_once "views/roles/admin/admin.view.php";
+                print_r($userDto);
 
             // if (isset($_SESSION['session'])) {
             //     $session = $_SESSION['session'];                
