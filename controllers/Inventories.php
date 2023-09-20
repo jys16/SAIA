fe38ff1dd87a06f9433d994c37feb2d000876274
jys16 <sflimas2@misena.edu.co>
2023-09-20 11:14:27 -0500

    Controlador de inventarios y reportes con sesiones

--- controllers/Inventories.php
+++ controllers/Inventories.php
@@ -1,26 +1,44 @@
-<?php
+<?php session_start();
+
+require_once "models/model_dto/UserDto.php";
+require_once "models/model_dao/UserDao.php";
+
     class Inventories{
-        public function __construct(){}
+
+        private $module;
+
+        public function __construct(){
+            $this->module = $_SESSION['module'];
+        }
         public function index(){
-            require_once "views/roles/admin/header.php";
-            require_once "views/roles/admin/admin_main.view.php";
-            require_once "views/roles/admin/footer.php";
+            header("Location: ?c=Dashboard");
         }        
         
         // Consultar stock
         public function readStock(){
             // Programar
-            require_once "views/roles/admin/header.php";            
-            require_once "views/modules/5_Inventory/stock_read.view.php";
-            require_once "views/roles/admin/footer.php";
-        }
-       
+            $userDto = unserialize($_SESSION['userDto']);
+            if (isset($_SESSION['userDto']) && ($userDto->getIdRol() <= 3 || $userDto->getIdRol() == 6)) {
+                require_once 'views/roles/'.$this->module.'/header.php';
+                require_once "views/modules/5_Inventory/stock_read.view.php";
+                require_once "views/roles/admin/footer.php";
+            }
+             else {
+                header("location:?");
+            }
+       }
         // Consultar Historia
         public function readHistory(){
-            // Programar
-            require_once "views/roles/admin/header.php";            
-            require_once "views/modules/5_Inventory/history_read.view.php";
-            require_once "views/roles/admin/footer.php";
-        }
+            $userDto = unserialize($_SESSION['userDto']);
+            if (isset($_SESSION['userDto']) && ($userDto->getIdRol() <= 3 || $userDto->getIdRol() == 6)) {
+                // Programar
+                require_once 'views/roles/'.$this->module.'/header.php';            
+                require_once "views/modules/5_Inventory/history_read.view.php";
+                require_once "views/roles/admin/footer.php";
+            }
+             else {
+            header("location:?");
+            }
+        }    
     }
 ?>
--- controllers/Reports.php
+++ controllers/Reports.php
@@ -1,26 +1,42 @@
-<?php
+<?php session_start();
+
+require_once "models/model_dto/UserDto.php";
+require_once "models/model_dao/UserDao.php";
+
     class Reports{
-        public function __construct(){}
+        public function __construct(){
+            $this->module = $_SESSION['module'];
+        }
         public function index(){
-            require_once "views/roles/admin/header.php";
-            require_once "views/roles/admin/admin_main.view.php";
-            require_once "views/roles/admin/footer.php";
+            header("Location: ?c=Dashboard");
         }        
         
         // Consultar stock
         public function reportGraphics(){
-            // Programar
-            require_once "views/roles/admin/header.php";            
-            require_once "views/modules/6_reports/report_graph.view.php";
-            // require_once "views/roles/admin/footer.php";
+            $userDto = unserialize($_SESSION['userDto']);
+            if (isset($_SESSION['userDto']) && ($userDto->getIdRol() <= 6)) {
+                // Programar
+                require_once 'views/roles/'.$this->module.'/header.php';            
+                require_once "views/modules/6_reports/report_graph.view.php";
+                // require_once "views/roles/admin/footer.php";
+            }
+             else {
+                header("location:?");
+            }
         }
        
         // Consultar Historia
         public function reportPrinted(){
-            // Programar
-            require_once "views/roles/admin/header.php";            
-            require_once "views/modules/6_reports/report_print.view.php";
-            require_once "views/roles/admin/footer.php";
+            $userDto = unserialize($_SESSION['userDto']);
+            if (isset($_SESSION['userDto']) && ($userDto->getIdRol() <= 6)) {
+                // Programar
+                require_once 'views/roles/'.$this->module.'/header.php';            
+                require_once "views/modules/6_reports/report_print.view.php";
+                require_once "views/roles/admin/footer.php";
+            }
+             else {
+                header("location:?");
+            }
         }
     }
 ?>
